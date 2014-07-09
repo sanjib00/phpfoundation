ee8cfe51d258a65c904501449f1febfddb2cccba
Sanjib <sanjib_1@hotmail.com>
2014-07-09 11:14:57 +0600

    My first program

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+echo "<h2>Hello FTFL!</h2>";
+echo "This is SANJIB <br>";
+echo "I'm learning PHP Basic now!<br>";
+//$aNumber=1234;
+//echo gettype($aNumber);
+//echo settype ($aNumber, 'String');
+//echo gettype ($aNumber); 
+//$_aString = abcd;
+//$_aString = 'abcd$aNumber';
+//$_anotherString = "abcd$aNumber";
+//echo gettype($_aString);
+//echo gettype($_anotherString);
+//echo $_aString;
+//echo "<br/>";
+//echo $_anotherString;
+
+//$aFloatedNumber = 12.12;
+//echo gettype ($aFloatedNumber);
+
+//$aBool = true;
+//$anotherBool = false;
+//echo gettype($aBool);
+//echo "<br/>";
+//echo gettype($anotherBool);
+$a=2*3+5;
+//echo $a "<br/>" . "\n";
+//echo nl2br($a."\n");
+//echo $a;
+$name='Sanjib';
+for($i=1; $i<=5; $i++)
+{
+
+?
+?
+
+?> 
