698db30ace64a5232a474f1821fd3c580a17a656
Sanjib <sanjib_1@hotmail.com>
2014-07-09 14:37:57 +0600

    not using logical AND

--- mark.php
+++ mark.php
@@ -1,33 +1,33 @@
 <?php
 
-$marks = 73;
+$marks = 120;
 
-if($marks >79) {
+if($marks >= 80) {
 
 echo "Grade is A+";
 }
 
-else if($marks>=70 && $marks < 80){
+else if($marks>=70){
 
 echo "Grade is A";
 }
 
-else if($marks>=60 && $marks < 70) {
+else if($marks>=60 ) {
 
 echo "Grade is A-";
 }
 
-else if($marks>=50 && $marks < 60) {
+else if($marks>=50) {
 
 echo "Grade is B";
 }
 
-else if($marks>=40 && $marks < 50) {
+else if($marks>=40) {
 
 echo "Grade is C";
 }
 
-else if($marks>=33 && $marks < 40) {
+else if($marks>=40) {
 
 echo "Grade is D";
 }
