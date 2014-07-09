47d72145cc33cbc2f26264c9f2a0b4742c2f9d0d
Sanjib <sanjib_1@hotmail.com>
2014-07-09 12:46:26 +0600

    update latest

--- operators.php
+++ operators.php
@@ -3,21 +3,44 @@
 $x = 100 / (2*5);
 $y = 100 % (2*5);
 $z = 100 / 2*5;
-$a = 0/5;
-$b = 0%5;
+//$a = 0/5;
+//$b = 0%5;
 
-echo $x."<br/>";
-echo $y."<br/>";
-echo $z."<br/>";
-echo $a."<br/>";
-echo $b."<br/>";
+//echo $x."<br/>";
+//echo $y."<br/>";
+//echo $z."<br/>";
+//echo $a."<br/>";
+//echo $b."<br/>";
 
 //increment / decrement
 //pre /post
-$a = 2;
-echo $a++."<br/>"; //2
-echo $a--."<br/>"; //3
-echo ++$a."<br/>"; //3
-echo --$a."<br/>"; //2
-echo $a."<br/>"; //2
+//$a = 2;
+//echo $a++."<br/>"; //2
+//echo $a--."<br/>"; //3
+//echo ++$a."<br/>"; //3
+//echo --$a."<br/>"; //2
+//echo $a."<br/>"; //2
+
+//$a= $a+1;
+//$a += 1;
+
+//echo $a.PHP_EOL;
+
+$a = 4;
+$b = 2;
+
+echo ++$a * ($a++ + --$b);
+
+$a =3;
+//$b = 0 ; // zero is false
+$b = 5 ;
+echo "<br/>";
+if(3==$a){
+	echo "Hello";
+}
+
+if(3 === $a){
+	echo "Hello";
+
+}
 ?>
